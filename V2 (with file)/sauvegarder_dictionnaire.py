b94cd63eacd62cb81b54d31f1e6ae1ffc7ce15eb
PatNwk <nowakp595@gmail.com>
2025-04-03 14:30:42 +0200

    bug

--- V2 (with file)/sauvegarder_dictionnaire.py
+++ V2 (with file)/sauvegarder_dictionnaire.py
@@ -4,5 +4,5 @@ FICHIER_DICO = "dictionnaire.json"
 
 
 def sauvegarder_dictionnaire(dictionnaire):
-    with open(FICHIER_DICO, "w", encoding="utf-8") as f:
+    with open(FICHIER_DICO, "w+", encoding="utf-8") as f:
         json.dump(dictionnaire, f, indent=4, ensure_ascii=False)
