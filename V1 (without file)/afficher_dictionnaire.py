74b9633dd497001cd9fb088f950323bf31652852
PatNwk <nowakp595@gmail.com>
2025-04-03 14:36:12 +0200

    Number in def

--- V1 (without file)/afficher_dictionnaire.py
+++ V1 (without file)/afficher_dictionnaire.py
@@ -2,6 +2,6 @@ def afficher_dictionnaire(dictionnaire):
     if dictionnaire:
         print("\nDictionnaire complet :")
         for mot, definition in sorted(dictionnaire.items()):
-            print(f"{mot} : {definition}")
+            print(f"{mot} : {len(definition.split())}")
     else:
         print("Le dictionnaire est vide.")
